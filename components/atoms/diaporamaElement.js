a7e164710c8dc2d5988dbb07c1cb2dc29878781e
moka <mokadev21@gmail.com>
2026-06-22 21:45:53 -0400

    caroussel wip v2

--- components/atoms/diaporamaElement.js
+++ components/atoms/diaporamaElement.js
@@ -1,8 +1,7 @@
 const diaporamaElement = (image) => {
     return `
-        <div class="w-auto h-auto bg-slate-900 p-4 rounded mb-4">
-            <h1 class="text-3xl font-bold mb-4 text-white absolute z-10">Welcome to My Website</h1>
-            <img src="${image.url}1980/1080" alt="${image.alt}" class=" w-1080 h-full mb-4 rounded-xl z-0">
+        <div class="p-4 rounded mb-4 flex-shrink-0 bg-image bg-cover bg-center relative bg-[url('${image.url}/1980/1080')] h-200 w-400">
+            <h1 class="text-3xl font-bold mb-4 text-white relative z-10">Welcome to My Website</h1>
         </div>
     `;
 };
--- components/molecules/diaporama.js
+++ components/molecules/diaporama.js
@@ -2,16 +2,16 @@ import diaporamaElement from "../atoms/diaporamaElement.js";
 
 
 var images = [
-    {"url": "https://picsum.photos/", "alt": "Random Image 1"},
-    {"url": "https://picsum.photos/", "alt": "Random Image 2"},
-    {"url": "https://picsum.photos/", "alt": "Random Image 3"},
-    {"url": "https://picsum.photos/", "alt": "Random Image 4"},
-    {"url": "https://picsum.photos/", "alt": "Random Image 5"}
+    {"url": "https://picsum.photos/", "alt": "header Image 1"},
+    {"url": "https://picsum.photos/", "alt": "header Image 2"},
+    {"url": "https://picsum.photos/", "alt": "header Image 3"},
+    {"url": "https://picsum.photos/", "alt": "header Image 4"},
+    {"url": "https://picsum.photos/", "alt": "header Image 5"}
 ];
 
 const diaporama = () => {
     return `
-    <div class="bg-slate-900 p-4 rounded mb-4 flex flex-row items-center gap-4 overflow-scroll ">
+    <div class="w-auto h-auto bg-slate-900 p-4 rounded flex flex-row items-center gap-4 overflow-scroll">
         ${images.map((image) => diaporamaElement(image)).join(" ")}
     </div>`;
 }
